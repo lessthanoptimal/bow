32ecf4aca10e2654723f3aaaef49bf00685abaa7
Peter Abeles <peter.abeles@gmail.com>
2014-12-30 19:19:00 -0600

    - better BOW-SIFT  * KD-Tree appears to be really slow

--- src/classifySiftImages.py
+++ src/classifySiftImages.py
@@ -2,6 +2,7 @@ __author__ = 'pja'
 
 from create_input_set import findLabeledImages
 from math import *
+from sklearn.neighbors import NearestNeighbors
 import scipy
 import random
 import pickle
@@ -9,9 +10,12 @@ import pickle
 from vlfeat import *
 import numpy as np
 
-sift_conf = {'step':20,'fast':True}
-kmeans_conf = {'K':50,'max_niters':100}
+sift_conf = {'step':8,'size':4,'fast':True}
+kmeans_conf = {'K':400,'num_seeds':1,'max_niters':200}
 maxTrainingImages = -1
+maxFeaturesPerImage = 150
+# number of neighbors in NN classifier
+numNeighbors = 10
 
 
 # Converts an image from integer to floating point format used by VLFeat
@@ -26,7 +30,7 @@ def convertImageToSingle(l):
 def compute_sift_words( filePath ):
     image = scipy.misc.imread(filePath)
     single = convertImageToSingle(image)
-    return vl_dsift(single,step=sift_conf['step'],fast=sift_conf['fast'])
+    return vl_dsift(single,step=sift_conf['step'],size=sift_conf['size'],fast=sift_conf['fast'])
 
 # Loads the entire training set and computes all the words in each image
 def computeAllWords( paths , verbose=False):
@@ -40,6 +44,13 @@ def computeAllWords( paths , verbose=False):
 
         [F,D]=compute_sift_words(filePath)
 
+        # randomly select N of them
+        N = min(D.shape[1],maxFeaturesPerImage)
+        set=range(D.shape[1])
+        random.shuffle(set)
+        D = D[:,set[0:N]]
+
+        # Add the new words onto the stack
         words = numpy.hstack([words,D])
 
     return words
@@ -68,7 +79,7 @@ def score_kclusters( dictionary , memberships , allWords ):
 
 
 # Use k-means clustering to find a simplified vocabulary from the set of words
-def create_vocabulary( words , max_iterations=20 , verbose=False):
+def create_vocabulary( words , verbose=False):
 
     if verbose:
         print 'total words = '+str(words.shape[1])
@@ -77,7 +88,9 @@ def create_vocabulary( words , max_iterations=20 , verbose=False):
     best_vocabulary = numpy.zeros((128,K),dtype=numpy.int32)
     best_score = float('Inf')
 
-    for i in xrange(max_iterations):
+    num_seeds = kmeans_conf['num_seeds']
+
+    for i in xrange(num_seeds):
         [C, I] = vl_ikmeans(words,K=K,max_niters=kmeans_conf['max_niters'])
         score = score_kclusters(C,I,words)
         if score < best_score:
@@ -90,7 +103,7 @@ def create_vocabulary( words , max_iterations=20 , verbose=False):
 
     return best_vocabulary
 
-# Finds the work in the vocabulary which is the best for the target
+# Finds the word in the vocabulary which is the best for the target
 def best_word( target , vocabulary , max_distance = float('inf')):
     best_word = -1
     best_score = float('inf')
@@ -103,62 +116,54 @@ def best_word( target , vocabulary , max_distance = float('inf')):
 
     return best_word
 
-def train_histogram( paths , vocabulary ):
-
+def compute_histogram( filePath , vocabulary ):
     K = kmeans_conf['K']
-
-    histograms = {}
-
-    for p in paths:
-        filePath = p['path']
-        label = p['label']
-
-        if label not in histograms:
-            histograms[label] = [0]*K
-        h = histograms[label]
-
-        # compute the words for each image
-        [F,D]=compute_sift_words(filePath)
-
-        for i in xrange(D.shape[1]):
-            bw = best_word(D[:,i],vocabulary)
-            if bw >= 0:
-                h[bw] += 1
-
-    # normalize the histograms so that they sum up to one
-    for key in histograms.keys():
-        h = histograms[key]
-        total = float(sum(h))
-        histograms[key] = [x/total for x in h]
-
-    return histograms
-
-def classifyImage( filePath , vocabulary , histograms ):
-    K = len(vocabulary)
 
     # find the words in the image
     [F,D]=compute_sift_words(filePath)
 
-    # compute normalized histogram
-    target = [0]*K
+    # TODO could compute histogram using distance here to improve accuracy?
+    # compute the histogram
+    histogram = [0]*K
     for i in xrange(D.shape[1]):
         bw = best_word(D[:,i],vocabulary)
         if bw >= 0:
-            target[bw] += 1
-    total = float(sum(target))
-    target = [x/total for x in target]
+            histogram[bw] += 1
+    # normalize the histogram so that it sums up to one
+    total = float(sum(histogram))
+    return [x/total for x in histogram]
 
-    # compare to labeled histograms
-    best_match = -1
-    best_score = 0
+def trainNearestNeighbor( paths , vocabulary, numLabels, numNeighbors , verbose=False ):
+    labels = [image['label'] for image in paths]
 
-    for key in histograms.keys():
-        score = np.multiply(target,histograms[key]).sum()
-        if score > best_score:
-            best_score = score
-            best_match = key
+    x = []
+    for p in paths:
+        filePath = p['path']
+        if verbose:
+            print 'training histogram for '+filePath
+        x.append( compute_histogram(filePath,vocabulary))
 
-    return best_match
+    x = np.array(x)
+    # nbrs = NearestNeighbors(n_neighbors=numNeighbors, algorithm='ball_tree').fit(x)
+    nbrs = NearestNeighbors(n_neighbors=numNeighbors, algorithm='kd_tree').fit(x)
+
+    return {'nbrs':nbrs,'labels':labels,'numLabels':numLabels}
+
+def classifyNearestNeighbor( filePath , vocabulary , classifierData ):
+    nbrs = classifierData['nbrs']
+    labels = classifierData['labels']
+    numLabels = classifierData['numLabels']
+
+    h = compute_histogram(filePath,vocabulary)
+
+    distance,indices = nbrs.kneighbors(h)
+
+    hits = [0]*numLabels
+    for idx,i in enumerate(indices.flat):
+        # hits[labels[i]] = hits[labels[i]] + 1 # uniform weighting
+        hits[labels[i]] = hits[labels[i]] + 1.0/(distance[0,idx]+0.2) # weight by distance
+
+    return np.argmax(hits)
 
 labeledTraining = findLabeledImages("../brown/data/train")
 
@@ -170,7 +175,7 @@ if maxTrainingImages > -1 and maxTrainingImages < len(labeledTraining['paths']):
 # Todo truncate
 
 dictionaryName = 'dictionary{0:d}.p'.format(kmeans_conf['K'])
-histogramsName = 'histograms{0:d}.p'.format(kmeans_conf['K'])
+nn_name = 'nndata{0:d}_NN{1:d}.p'.format(kmeans_conf['K'],numNeighbors)
 
 try:
     vocabulary = pickle.load(open( dictionaryName, "rb" ))
@@ -179,19 +184,19 @@ except IOError:
     print 'Computing words'
     allWords = computeAllWords(labeledTraining['paths'],True)
     print 'Creating the vocabulary'
-    vocabulary = create_vocabulary(allWords,5,True)
+    vocabulary = create_vocabulary(allWords,True)
     pickle.dump(vocabulary,open( dictionaryName, "wb" ))
     allWords = []
 except:
     raise
 
 try:
-    histograms = pickle.load(open( histogramsName, "rb" ))
-    print 'Loaded histogram! file name = '+histogramsName
+    nn_data = pickle.load(open( nn_name, "rb" ))
+    print 'Loaded NN graph! file name = '+nn_name
 except IOError:
-    print 'Computing histograms from training set'
-    histograms = train_histogram(labeledTraining['paths'],vocabulary)
-    pickle.dump(histograms,open( histogramsName, "wb" ))
+    print 'Computing histograms and NN graph from training set'
+    nn_data = trainNearestNeighbor(labeledTraining['paths'],vocabulary,labeledTraining['numLabels'],numNeighbors,False)
+    pickle.dump(nn_data,open( nn_name, "wb" ))
 except:
     raise
 
@@ -208,19 +213,23 @@ for p in labeledTest['paths']:
     filePath = p['path']
     label = p['label']
     print 'Scoring '+filePath
-    found = classifyImage(filePath,vocabulary,histograms)
+    found = classifyNearestNeighbor(filePath,vocabulary,nn_data)
     total += 1
-    if found is label:
+    if found == label:
         totalCorrect += 1
-        # print'    correct! label = '+str(found)
-    # else:
-    #     print'    incorrect: expected '+str(label)+"  found "+str(found)
+        print'    correct! label = '+str(found)
+    else:
+        print'    incorrect: expected '+str(label)+"  found "+str(found)
 
 print 'Faction Correct: '+str(totalCorrect/float(total))
 
 
 print 'Done!'
 
-# TODO Compute histogram for all training images.  Save labeled results on a per image absis
+# TODO Benchmark KD-Tree versus brute force
+# TODO explore "better" clustering on results.  Adjust K
+# TODO Create a generic class for classification
+# TODO Linear SVM classifier
+# TODO create a confusion matrix from results
 
 # save results for the found vocabulary!!!
